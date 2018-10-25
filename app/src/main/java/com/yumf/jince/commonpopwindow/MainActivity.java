c8a3f8d587b509f7008c52b55bd8790bc57f2b08
minfeng.yu <minfeng.yu@yintech.cn>
2018-10-25 11:13:22 +0800

    add basic method

--- app/src/main/java/com/yumf/jince/commonpopwindow/MainActivity.java
+++ app/src/main/java/com/yumf/jince/commonpopwindow/MainActivity.java
@@ -1,25 +1,38 @@
 package com.yumf.jince.commonpopwindow;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.yumf.jince.library.CommonPopupWindow;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private TextView textViewB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.normal).setOnClickListener(new View.OnClickListener() {
+        textView = findViewById(R.id.normal);
+        textViewB = findViewById(R.id.text);
+
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                CommonPopupWindow commonPopupWindow = new CommonPopupWindow(MainActivity.this, R.layout.popupwindow_layout, 400, 300);
+                CommonPopupWindow commonPopupWindow = new CommonPopupWindow(MainActivity.this, R.layout.popupwindow_layout, R.color.colorAccent);
                 commonPopupWindow.showAsDropDown(findViewById(R.id.normal));
+
+//                commonPopupWindow.showAtLocation(textView, Gravity.TOP | Gravity.LEFT,0,0);
+                textViewB.setText("测试");
             }
         });
     }
--- library/src/main/java/com/yumf/jince/library/CommonPopupWindow.java
+++ library/src/main/java/com/yumf/jince/library/CommonPopupWindow.java
@@ -1,8 +1,11 @@
 package com.yumf.jince.library;
 
 import android.content.Context;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.LayoutRes;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.PopupWindow;
 
 /**
@@ -11,8 +14,15 @@ import android.widget.PopupWindow;
 
 public class CommonPopupWindow extends PopupWindow {
 
+    public CommonPopupWindow(Context context, @LayoutRes int layout) {
+        this(context, layout, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, android.R.color.transparent);
+    }
 
-    public CommonPopupWindow(Context context, @LayoutRes int layout, int width, int height) {
+    public CommonPopupWindow(Context context, @LayoutRes int layout, @DrawableRes int background) {
+        this(context, layout, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, background);
+    }
+
+    public CommonPopupWindow(Context context, @LayoutRes int layout, int width, int height, @DrawableRes int background) {
         super(context);
 
         setFocusable(true);
@@ -22,6 +32,8 @@ public class CommonPopupWindow extends PopupWindow {
         setWidth(width);
         setHeight(height);
 
+        //背景透明
+        setBackgroundDrawable(ContextCompat.getDrawable(context, background));
         // 设置弹窗的布局界面
         setContentView(LayoutInflater.from(context).inflate(layout, null));
     }
